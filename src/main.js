abdfbee81968f1cdba3535be47fad066a99ece46
Subodh Shah <shahsubodh166@gmail.com>
2024-10-30 12:51:59 +0545

    made booking button

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import './main.css';
 import { gsap } from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import progressIcon from './components/progressicon.js';
 import drawerToggle from './components/drawer.js';
-// gsap.registerPlugin(ScrollTrigger);
 
 function heroInfo() {
   const tl = gsap.timeline();
-  tl.from('.hero-info h1', {
-    x: -500,
+  tl.from('.hero-para', {
+    y: -75,
     opacity: 0,
     duration: 2,
-    ease: 'sine',
+    ease: 'power1.inOut',
+  });
+  tl.from('.hero-info h1', {
+    x: -100,
+    opacity: 0,
+    duration: 2,
+    ease: 'power2.inOut',
   });
 }
 function main() {
